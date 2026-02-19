7c17aef04aea302cde5af86e86ad0b4e0e996f1c
MD JAVED ALI <mdjavedali67990@gmail.com>
2026-02-19 13:19:49 +0530

    started first code with some input output functions.

---
+++ Introduction/Hello.java
@@ -0,0 +1,11 @@
+package Introduction;
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.next());
+    }
+}
